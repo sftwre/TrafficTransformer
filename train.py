9f3d81ddc505777f8d1800e5aaafb879c5312193
Isaac <isaac.buitrago@utexas.edu>
2025-04-04 16:05:12 -0500

    Added cli args
    - Added cli args to training script for dynamic experiment configuration

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from model import TrafficTransformer
 from loss import TrafficLoss
+import argparse
 
 
 def train(model, dataloader, loss_fn, optimizer, device="cpu"):
@@ -64,53 +65,98 @@ def eval(model, dataloader, loss_fn, device="cpu"):
     return eval_loss
 
 
-data_base_dir = Path("./nexar-collision-prediction")
-df_train = pd.read_csv(data_base_dir / "sample.csv")
-df_train["time_of_event"] = df_train["time_of_event"].fillna(0)
-df_train["time_of_alert"] = df_train["time_of_alert"].fillna(0)
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Train a Transformer model to predict crash events and event time."
+    )
+    parser.add_argument(
+        "--num_epochs", type=int, default=5, help="Number of training epochs"
+    )
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=5e-3,
+        help="Learning rate for the optimizer",
+    )
+    parser.add_argument(
+        "--k", type=int, default=2, help="Number of cross-validation folds"
+    )
+    parser.add_argument(
+        "--image_size", type=int, default=224, help="Input image dimension for training"
+    )
+    parser.add_argument(
+        "--n_frames",
+        type=int,
+        default=16,
+        help="Number of image frames to sample from videos",
+    )
+    parser.add_argument(
+        "--n_workers",
+        type=int,
+        default=4,
+        help="Number of processes to use for data loading",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=8,
+        help="Number of train/val samples in each batch",
+    )
+    args = parser.parse_args()
+
+    data_base_dir = Path("./nexar-collision-prediction")
+    df_train = pd.read_csv(data_base_dir / "train.csv")
+
+    df_train["time_of_event"] = df_train["time_of_event"].fillna(0)
+    df_train["time_of_alert"] = df_train["time_of_alert"].fillna(0)
 
 
 annotations = get_annotations(df_train)
-video_dir = data_base_dir / "train"
-video_ids = list(annotations.keys())
-processed_data = parallel_preprocess_dataset(
-    video_dir, video_ids, num_frames=32, image_size=240, num_workers=4
-)
+    annotations = get_annotations(df_train)
 
-# model init
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-model = TrafficTransformer(image_size=240).to(device)
-
+    video_dir = data_base_dir / "train"
+    video_ids = list(annotations.keys())
+    processed_data = parallel_preprocess_dataset(
+        video_dir,
+        video_ids,
+        num_frames=args.n_frames,
+        image_size=args.image_size,
+        num_workers=args.n_workers,
+    )
 optimizer = torch.optim.Adam(model.parameters(), lr=5e-3)
 
-loss_fn = TrafficLoss()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    loss_fn = TrafficLoss()
 
 
-# Set up k-fold validation
-k = 5  # Number of folds
-kf = KFold(n_splits=k, shuffle=True, random_state=42)
+    # Set up k-fold validation
+    kf = KFold(n_splits=args.k, shuffle=True, random_state=42)
 
-# Example: Iterate through each fold
-for fold, (train_idx, val_idx) in enumerate(kf.split(df_train)):
-    train_data = df_train.iloc[train_idx]
-    val_data = df_train.iloc[val_idx]
 
-    train_annons = dict()
-    train_frames = dict()
+    for fold, (train_idx, val_idx) in enumerate(kf.split(df_train)):
 
-    for id in train_data.id.tolist():
-        id = f"{id:05d}"
-        train_annons[id] = annotations[id]
-        train_frames[id] = processed_data[id]
+        # model init
+        model = TrafficTransformer(image_size=args.image_size).to(device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+        train_data = df_train.iloc[train_idx]
+        val_data = df_train.iloc[val_idx]
 
-    val_annons = dict()
-    val_frames = dict()
+        train_annons = dict()
+        train_frames = dict()
 
-    for id in val_data.id.tolist():
-        id = f"{id:05d}"
-        val_annons[id] = annotations[id]
-        val_frames[id] = processed_data[id]
+        for id in train_data.id.tolist():
+            id = f"{id:05d}"
+            train_annons[id] = annotations[id]
+            train_frames[id] = processed_data[id]
+
+        val_annons = dict()
+        val_frames = dict()
+
+        for id in val_data.id.tolist():
+            id = f"{id:05d}"
+            val_annons[id] = annotations[id]
+            val_frames[id] = processed_data[id]
 
         train_dataset = DashcamDataset(
             processed_data=train_frames,
@@ -123,12 +169,24 @@ for fold, (train_idx, val_idx) in enumerate(kf.split(df_train)):
             transform=basic_transforms,
         )
 
-    train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=True)
+        train_dataloader = DataLoader(
+            train_dataset, batch_size=args.batch_size, shuffle=True
+        )
+        val_dataloader = DataLoader(
+            val_dataset, batch_size=args.batch_size, shuffle=True
+        )
 
     train_loss = train(model, train_dataloader, loss_fn, optimizer, device=device)
     val_loss = eval(model, val_dataloader, loss_fn, device=device)
 
-    print(
-        f"Fold [{fold+1}/{k}] -> train loss: {train_loss:.3f}, val loss: {val_loss:.3f}"
-    )
+        # train model on K-1 folds
+        for epoch in range(args.num_epochs):
+
+            train_loss = train(
+                model,
+                train_dataloader,
+                loss_fn,
+                optimizer,
+                device=device,
+            )
+            val_loss = eval(model, val_dataloader, loss_fn, device=device)
